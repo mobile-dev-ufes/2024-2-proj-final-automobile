e1a6d1f27a9ab14ba656acbe576bf1a2342b2f3b
naascimento0 <gabrieloliveiran@hotmail.com>
2025-03-08 14:21:36 -0300

    gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    // Hilt
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    // Room
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+}
