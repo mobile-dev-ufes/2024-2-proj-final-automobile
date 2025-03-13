9272567b45bb5f52d7c82595c8af98ab3208306d
naascimento0 <gabrieloliveiran@hotmail.com>
2025-03-13 14:18:31 -0300

    theme, color and fonts

--- app/src/main/java/com/ufes/automobile/ui/theme/Color.kt
+++ app/src/main/java/com/ufes/automobile/ui/theme/Color.kt
@@ -2,12 +2,8 @@ package com.ufes.automobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val Blue = Color(0xFF0C45A0)
+val BluePrimary = Color(0xFF0C45A0)
+val LightBlue = Color(0xFF62b3f5)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Yellow = Color(0xFFffd767)
--- app/src/main/java/com/ufes/automobile/ui/theme/Type.kt
+++ app/src/main/java/com/ufes/automobile/ui/theme/Type.kt
@@ -2,33 +2,47 @@ package com.ufes.automobile.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ufes.automobile.R
+
+val RobotoMonoFamily = FontFamily(
+//    Font(R.font.roboto_mono_regular, FontWeight.Normal),
+//    Font(R.font.roboto_mono_bold, FontWeight.Bold),
+//    Font(R.font.roboto_mono_medium, FontWeight.Medium)
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_medium, FontWeight.Medium)
+
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = RobotoMonoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = TextStyle(
+        fontFamily = RobotoMonoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = RobotoMonoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = RobotoMonoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = RobotoMonoFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp
     )
-    */
 )
