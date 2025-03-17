b412b21353981c8b42acdf3249f61cc7b9e79f3f
PhenBD <pedrinho_phbd@hotmail.com>
2025-03-16 21:43:28 -0300

    tela de estatistica com distancia percorrida nos ultimos 6 meses + divisao de custos

--- app/src/main/java/com/ufes/automobile/ui/reports/ReportsScreen.kt
+++ app/src/main/java/com/ufes/automobile/ui/reports/ReportsScreen.kt
@@ -1,13 +1,17 @@
 package com.ufes.automobile.ui.reports
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,21 +23,27 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.ufes.automobile.ui.theme.AutoMobileTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReportsScreen(
     vehicleId: Int?,
@@ -44,9 +54,26 @@ fun ReportsScreen(
         vehicleId?.let { viewModel.loadStatistics(it) }
     }
 
-    val distanceData by viewModel.distanceData.collectAsState()
-    val costData by viewModel.costData.collectAsState()
+    val distanceData = viewModel.distanceData.collectAsState().value
+    val costData = viewModel.costData.collectAsState().value
+    val months = viewModel.months.collectAsState().value
 
+    ReportsContent(
+        onBackClick = { navController.popBackStack() },
+        distanceData = distanceData,
+        costData = costData,
+        months = months
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReportsContent(
+    onBackClick: () -> Unit,
+    distanceData: List<BarEntry>,
+    costData: List<PieEntry>,
+    months: Set<String>,
+){
     Scaffold(
         topBar = {
             TopAppBar(
@@ -58,7 +85,7 @@ fun ReportsScreen(
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
+                    IconButton(onClick = onBackClick) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Back",
@@ -76,83 +103,170 @@ fun ReportsScreen(
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .padding(16.dp)
+                .padding(horizontal = 8.dp)
                 .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Text(
-                text = "Distance Traveled Over Time",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            AndroidView(
+            Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp),
-                factory = { context ->
-                    LineChart(context).apply {
-                        description.isEnabled = false
-                        setTouchEnabled(true)
-                        isDragEnabled = true
-                        setScaleEnabled(true)
-                        setPinchZoom(true)
+                    .padding(16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = "Distance Traveled Monthly (km)",
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    AndroidView(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp),
+                        factory = { context ->
+                            BarChart(context).apply {
+                                description.isEnabled = false // Remover a descrição
+                                legend.isEnabled = false
+                                //setTouchEnabled(true) // Permitir interação com o usuário
+                                //isDragEnabled = true // Permitir arrastar o gráfico
+                                //setScaleEnabled(true) // Permitir zoom
+                                //setPinchZoom(true) // Permitir zoom com dois dedos
+                                // Outras configurações do gráfico podem vir aqui
+                                val barDataSet = BarDataSet(distanceData, "Distância (km)") // Rótulo do conjunto de dados
 
-                        val lineDataSet = LineDataSet(distanceData, "Distance (km)")
-                        lineDataSet.colors = ColorTemplate.MATERIAL_COLORS.toList()
-                        lineDataSet.setDrawValues(false)
+                                barDataSet.color = ContextCompat.getColor(context, android.R.color.holo_blue_dark) // Cor do conjunto de dados
 
-                        val lineData = LineData(lineDataSet)
-                        data = lineData
-                        invalidate()
-                    }
-                },
-                update = { chart ->
-                    val lineDataSet = LineDataSet(distanceData, "Distance (km)")
-                    lineDataSet.colors = ColorTemplate.MATERIAL_COLORS.toList()
-                    lineDataSet.setDrawValues(false)
+                                xAxis.textSize = 14f
+                                xAxis.setDrawGridLines(false)
+                                xAxis.position = XAxis.XAxisPosition.BOTTOM
 
-                    val lineData = LineData(lineDataSet)
-                    chart.data = lineData
-                    chart.invalidate()
+                                val barData = BarData(barDataSet)
+                                data = barData
+
+                                animateY(1000) // Animação de entrada
+
+                                invalidate() // Atualizar o gráfico
+                            }
+                        },
+                        update = { chart ->
+                            val barDataSet = BarDataSet(distanceData, "Distância (km)")
+
+                            val xAxisVals = mutableListOf<String>()
+                            months.forEachIndexed { index, value ->
+                                xAxisVals.add(index, value)
+                            }
+
+                            val xAxisFormatter = IndexAxisValueFormatter(xAxisVals)
+                            chart.xAxis.valueFormatter = xAxisFormatter
+
+                            val barData = BarData(barDataSet)
+                            chart.data = barData
+                            chart.invalidate()
+                        }
+                    )
                 }
-            )
-
-            Text(
-                text = "Cost Distribution",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            AndroidView(
+            }
+            Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp),
-                factory = { context ->
-                    PieChart(context).apply {
-                        description.isEnabled = false
-                        setUsePercentValues(true)
-                        isDrawHoleEnabled = true
-                        holeRadius = 40f
-                        transparentCircleRadius = 45f
+                    .padding(16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = "Cost Distribution",
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    AndroidView(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        factory = { context ->
+                            PieChart(context).apply {
+                                description.isEnabled = false
+                                setUsePercentValues(true)
+                                isDrawHoleEnabled = true
+                                holeRadius = 40f
+                                transparentCircleRadius = 45f
 
-                        val pieDataSet = PieDataSet(costData, "Costs")
-                        pieDataSet.colors = ColorTemplate.JOYFUL_COLORS.toList()
-                        pieDataSet.setDrawValues(true)
+                                legend.isEnabled = true // Garante que a legenda esteja visível
+                                legend.textSize = 14f
 
-                        val pieData = PieData(pieDataSet)
-                        data = pieData
-                        invalidate()
-                    }
-                },
-                update = { chart ->
-                    val pieDataSet = PieDataSet(costData, "Costs")
-                    pieDataSet.colors = ColorTemplate.JOYFUL_COLORS.toList()
-                    pieDataSet.setDrawValues(true)
+                                val pieDataSet = PieDataSet(costData, "Costs")
+                                pieDataSet.colors = ColorTemplate.LIBERTY_COLORS.toList()
+                                pieDataSet.setDrawValues(false)
+                                setDrawEntryLabels(false)
 
-                    val pieData = PieData(pieDataSet)
-                    chart.data = pieData
-                    chart.invalidate()
+                                val pieData = PieData(pieDataSet)
+                                data = pieData
+                                invalidate()
+                            }
+                        },
+                        update = { chart ->
+                            chart.setDrawEntryLabels(false)
+                            chart.legend.isEnabled = true // Garante que a legenda esteja visível
+                            chart.legend.textSize = 14f
+                            val pieDataSet = PieDataSet(costData, "Costs")
+                            pieDataSet.colors = ColorTemplate.MATERIAL_COLORS.toList()
+                            pieDataSet.setDrawValues(false)
+
+                            val pieData = PieData(pieDataSet)
+                            chart.data = pieData
+                            chart.invalidate()
+                        }
+                    )
                 }
-            )
+            }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ReportsScreenPreview() {
+    val distanceData = listOf(
+        BarEntry(0f, 100f), // Janeiro: 100 km
+        BarEntry(1f, 150f), // Fevereiro: 150 km
+        BarEntry(2f, 200f), // Março: 200 km
+        BarEntry(3f, 180f), // Abril: 180 km
+        BarEntry(4f, 220f), // Maio: 220 km
+        BarEntry(5f, 250f), // Junho: 250 km
+    )
+    val costData = listOf(
+        PieEntry(300f, "Fuel/Charging"), // 300 unidades em combustível/carga
+        PieEntry(150f, "Maintenance"),   // 150 unidades em manutenção
+        PieEntry(50f, "Insurance"),      // 50 unidades em seguro
+    )
+    val months = setOf("Jan", "Fev", "Mar", "Abr", "Mai", "Jun")
+
+    AutoMobileTheme {
+        ReportsContent(
+            onBackClick = {},
+            distanceData = distanceData,
+            costData = costData,
+            months = months
+        )
     }
 }
--- app/src/main/java/com/ufes/automobile/ui/reports/ReportsViewModel.kt
+++ app/src/main/java/com/ufes/automobile/ui/reports/ReportsViewModel.kt
@@ -1,43 +1,131 @@
 package com.ufes.automobile.ui.reports
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieEntry
+import com.ufes.automobile.domain.repository.DisplacementRepository
 import com.ufes.automobile.domain.repository.GarageRepository
+import com.ufes.automobile.domain.repository.InsuranceRepository
+import com.ufes.automobile.domain.repository.MaintenanceRepository
+import com.ufes.automobile.domain.repository.RechargeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.util.Calendar
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class ReportsViewModel @Inject constructor(
-    private val garageRepository: GarageRepository // Supondo que você tenha um repositório
+    private val displacementRepository: DisplacementRepository,
+    private val rechargeRepository: RechargeRepository,
+    private val maintenanceRepository: MaintenanceRepository,
+    private val insuranceRepository: InsuranceRepository
 ) : ViewModel() {
-    private val _distanceData = MutableStateFlow<List<Entry>>(emptyList()) // Corrigido para Entry do MPAndroidChart
-    val distanceData: StateFlow<List<Entry>> = _distanceData.asStateFlow() // Corrigido para Entry do MPAndroidChart
+    private val _distanceData = MutableStateFlow<List<BarEntry>>(emptyList()) // Corrigido para Entry do MPAndroidChart
+    val distanceData: StateFlow<List<BarEntry>> = _distanceData.asStateFlow() // Corrigido para Entry do MPAndroidChart
 
     private val _costData = MutableStateFlow<List<PieEntry>>(emptyList())
     val costData: StateFlow<List<PieEntry>> = _costData.asStateFlow()
+
+    private val _months = MutableStateFlow<Set<String>>(emptySet())
+    val months: StateFlow<Set<String>> = _months.asStateFlow()
 
     fun loadStatistics(vehicleId: Int) {
         viewModelScope.launch {
             // Simulação de dados (substitua pela lógica real do seu repositório)
             // Exemplo: Distância percorrida por mês
-            _distanceData.value = listOf(
-                Entry(1f, 100f), // Janeiro: 100 km
-                Entry(2f, 150f), // Fevereiro: 150 km
-                Entry(3f, 200f), // Março: 200 km
-                Entry(4f, 180f), // Abril: 180 km
-            )
+            var displacements = displacementRepository.getDisplacementsByVehicle(vehicleId)
+            displacements = displacements.sortedBy { it.date }
 
-            // Exemplo: Distribuição de custos
+            // Calculate the timestamp 6 months ago
+            val sixMonthsAgo = Calendar.getInstance()
+            sixMonthsAgo.add(Calendar.MONTH, -5) // Subtract 6 months
+
+            // Filter the displacements
+            displacements = displacements.filter { displacement ->
+                val displacementDate = Calendar.getInstance().apply {
+                    time = Date(displacement.date)
+                }
+                displacementDate.after(sixMonthsAgo) or (displacementDate.get(Calendar.MONTH) == sixMonthsAgo.get(Calendar.MONTH))
+            }
+
+            var months = mutableSetOf<String>()
+            var distancePerMonth = mutableListOf<Float>()
+            for (i in 0..11) {
+                distancePerMonth.add(i, 0f)
+            }
+
+            displacements.forEach{displacement ->
+                val month = Calendar.getInstance().apply { time = Date(displacement.date) }.get(Calendar.MONTH)
+                distancePerMonth[month] += displacement.distance
+                when (month) {
+                    0 -> months.add("Jan")
+                    1 -> months.add("Fev")
+                    2 -> months.add("Mar")
+                    3 -> months.add("Abr")
+                    4 -> months.add("Mai")
+                    5 -> months.add("Jun")
+                    6 -> months.add("Jul")
+                    7 -> months.add("Ago")
+                    8 -> months.add("Set")
+                    9 -> months.add("Out")
+                    10 -> months.add("Nov")
+                    11 -> months.add("Dez")
+                }
+            }
+
+            _months.value = months
+
+            var distancePerMonthFiltered = mutableListOf<BarEntry>()
+            val now = Calendar.getInstance()
+            var i = sixMonthsAgo.get(Calendar.MONTH)
+            var k = 0.0f
+            var g = 0
+            if (now.get(Calendar.MONTH) + 1 > 11)
+                g = 0
+            else
+                g = now.get(Calendar.MONTH) + 1
+
+            while (i != g) {
+                distancePerMonthFiltered.add(BarEntry(k, distancePerMonth[i]))
+                i++
+                if (i > 11)
+                    i = 0
+                k++
+            }
+
+            _distanceData.value = distancePerMonthFiltered
+
+            var recharges = rechargeRepository.getRechargesByVehicle(vehicleId)
+            var maintenances = maintenanceRepository.getMaintenanceByVehicle(vehicleId)
+            var insurances = insuranceRepository.getInsuranceByVehicle(vehicleId)
+
+            var totalRecharges = 0f
+            var totalMaintenances = 0f
+            var totalInsurances = 0f
+
+            recharges.forEach { recharge ->
+                totalRecharges += recharge.cost
+            }
+
+            maintenances.forEach { maintenance ->
+                totalMaintenances += maintenance.cost
+            }
+
+            insurances.forEach { insurance ->
+                totalInsurances += insurance.cost
+            }
+
             _costData.value = listOf(
-                PieEntry(300f, "Fuel/Charging"), // 300 unidades em combustível/carga
-                PieEntry(150f, "Maintenance"),   // 150 unidades em manutenção
-                PieEntry(50f, "Insurance"),      // 50 unidades em seguro
+                PieEntry(totalRecharges, "Fuel/Charging"),
+                PieEntry(totalMaintenances, "Maintenance"),
+                PieEntry(totalInsurances, "Insurance")
             )
         }
     }
